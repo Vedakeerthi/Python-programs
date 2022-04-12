b2e387e0ba446b4417575fc1d2b275adf8535026
Vedakeerthi A <77675280+Vedakeerthi@users.noreply.github.com>
2022-04-12 18:24:23 +0530

    Initial commit

---
+++ RANDOM NUMBER GAME.py
@@ -0,0 +1,14 @@
+import random as r
+def randg(i):
+    n = r.randint(1,10)
+    if n>i and n<i:
+        print("NOT EVEN CLOSE, TRY AGAIN!!")
+    elif n==i+1 or n==i-1:
+        print("SOO CLOSE!! THE NUMBER IS ",n)
+    else:
+        print("YAY!! CONGRATS YOU FOUND IT!!")
+
+if __name__ == '__main__':
+    print("----------RANDOM NUMBER GAME----------")
+    a = int(input("ENTER A NUMBER FROM 1-10 : "))
+    randg(a)
